1bbb2727a43ff15917e75de8938cccea6370ebcd
Paweł Piekutowski <futureiscode@gmail.com>
2019-05-15 14:32:38 +0200

    Dodanie klasy random

--- Lekcja6.1/Program.cs
+++ Lekcja6.1/Program.cs
@@ -10,6 +10,22 @@ namespace Lekcja6._1
     {
         static void Main(string[] args)
         {
+            Random rnd = new Random();
+
+            int los = rnd.Next(1,11);
+            int mojaLiczba;
+
+
+            do
+            {
+                Console.WriteLine("Zgadnij liczbę z Zakresu 1-10");
+                Console.WriteLine("Podaj liczbę: ");
+                mojaLiczba = int.Parse(Console.ReadLine());
+
+            } while (los != mojaLiczba);
+
+
+            Console.ReadLine();
         }
     }
 }
