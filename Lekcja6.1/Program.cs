612e36e5a61c8774e99d31ac6f66afec5ef2f5c3
Paweł Piekutowski <futureiscode@gmail.com>
2019-05-15 14:39:26 +0200

    Dodanie instrukcji warunkowych

--- Lekcja6.1/Program.cs
+++ Lekcja6.1/Program.cs
@@ -15,12 +15,25 @@ namespace Lekcja6._1
             int los = rnd.Next(1,11);
             int mojaLiczba;
 
-
+            Console.WriteLine("Zgadnij liczbę z Zakresu 1-10");
             do
             {
-                Console.WriteLine("Zgadnij liczbę z Zakresu 1-10");
+                
                 Console.WriteLine("Podaj liczbę: ");
                 mojaLiczba = int.Parse(Console.ReadLine());
+
+                if (mojaLiczba >los)
+                {
+                    Console.WriteLine("Podałeś zbyt dużą liczbę!");
+                }
+                else if(mojaLiczba < los)
+                {
+                    Console.WriteLine("Podałeś zbyt małą liczbę");
+                }
+                else
+                {
+                    Console.WriteLine("Brawo odgadłeś moją liczbę");
+                }
 
             } while (los != mojaLiczba);
 
